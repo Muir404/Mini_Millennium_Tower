38594822052f1973799a1ca003cc9395c9581e44
XuMao <15851628332@163.com>
2026-03-08 22:00:39 +0800

    修改部分问题

--- src/engine/component/render_component.h
+++ src/engine/component/render_component.h
@@ -4,13 +4,12 @@ namespace engine::component
 {
     struct RenderComponent
     {
-        int layer_{};   ///< 渲染层，越大越后渲染
-        float depth_{}; ///< 渲染深度，越小越先渲染
+        int layer_{};   ///< 图层ID，越小越先渲染
+        float depth_{}; ///< 同一图层内的深度，越小越先渲染
 
         RenderComponent(int layer = 0, float depth = 0.0f) : layer_(layer), depth_(depth) {}
 
         // 重载比较运算符，用于排序
-        // auto operator<=>(const RenderComponent &other) const = default; ///< 渲染组件的比较运算符，先按层排序，再按深度排序 C++20 特性
         bool operator<(const RenderComponent &other) const
         {
             if (layer_ == other.layer_)
